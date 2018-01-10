cb29621a67759b2b67f0d0fa81ae72bb7498612b
Abhinav Krishna C K <me@abhy.me>
2018-01-10 23:11:49 +0530

    Added youtube API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import YTSearch from 'youtube-api-search'
+
 import SearchBar from './components/search_bar'
 
 const API_KEY = 'AIzaSyD67xaxJL9dj-k-oJpr5hxgx1h27X57wXY';
+
+YTSearch({key: API_KEY, term: 'Abllea'}, (data) => {
+  console.log(data);
+})
+
+
 
 const App = () => {
   return (<div>
